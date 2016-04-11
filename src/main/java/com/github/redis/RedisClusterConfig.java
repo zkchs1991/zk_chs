3ce53831f13db10ed43ebff1916320cb7ef08f89
zk_chs <zkchs1991@gmail.com>
2016-04-11 17:47:26 +0800

    bean -> StringRedisTemplate

--- src/main/java/com/github/redis/RedisClusterConfig.java
+++ src/main/java/com/github/redis/RedisClusterConfig.java
@@ -7,6 +7,7 @@ import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisClusterConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * Created by zk_chs on 16/4/11.
@@ -26,10 +27,22 @@ public class RedisClusterConfig {
                 new RedisClusterConfiguration(clusterProperties.getNodes()));
     }
 
+    /**
+     * 对redis的存取,据需要先行转换成byte[]
+     * */
     @Bean
     @Autowired
     public RedisClusterConnection redisClusterConnection (RedisConnectionFactory connectionFactory){
         return connectionFactory.getClusterConnection();
+    }
+
+    /**
+     * 该bean可以直接操作String,无需转成byte[]
+     * */
+    @Bean
+    @Autowired
+    public StringRedisTemplate stringRedisTemplate (RedisConnectionFactory connectionFactory){
+        return new StringRedisTemplate(connectionFactory);
     }
 
 }
