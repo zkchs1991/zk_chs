e67269f1334187e9ec386161c5c24e6f452aa118
zk_chs <zkchs1991@gmail.com>
2016-04-13 18:45:05 +0800

    scala与spring一起使用时，请求资源需要添加返回值才行

--- src/main/java/com/github/config/RedisSessionConfig.java
+++ src/main/java/com/github/config/RedisSessionConfig.java
@@ -4,11 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
- * Created by zk_chs on 16/4/11.
- * 这个配置是用来开启session共享的,使用redis存储session
- * redis连接配置位置在application.properties中
+ * Created by zk_chs on 16/4/13.
  */
 @Configuration
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)/** 默认即为1800s */
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800) /** 默认即为1800s */
 public class RedisSessionConfig {
+
 }
