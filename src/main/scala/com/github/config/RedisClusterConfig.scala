06d03bfcdafac0488e49f966ef19452f9a661e12
zk_chs <zkchs1991@gmail.com>
2016-04-15 11:54:21 +0800

    redis全部改用scala完成

--- src/main/scala/com/github/config/RedisClusterConfig.scala
+++ src/main/scala/com/github/config/RedisClusterConfig.scala
@@ -1,6 +1,5 @@
 package com.github.config
 
-import com.github.redis.RedisClusterConfigProperties
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.data.redis.connection.{RedisClusterConnection, RedisClusterConfiguration, RedisConnectionFactory}
---
+++ src/main/scala/com/github/utils/GsonUtil.scala
@@ -0,0 +1,12 @@
+package com.github.utils
+
+import com.google.gson.Gson
+
+/**
+  * Created by zk_chs on 16/4/15.
+  */
+object GsonUtil {
+
+  val GSON: Gson = new Gson
+
+}
