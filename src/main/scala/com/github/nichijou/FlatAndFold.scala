e0631fe252274b2313cce7d9b146daa7d3e5300a
zk_chs <zkchs1991@gmail.com>
2016-04-24 23:28:18 +0800

    scala flatMap and foldLeft

---
+++ src/main/scala/com/github/nichijou/FlatAndFold.scala
@@ -0,0 +1,31 @@
+package com.github.nichijou
+
+/**
+  * Created by zk_chs on 16/4/24.
+  */
+object FlatAndFold extends App {
+
+  println(List.range(1,2)) // 不会取到2
+
+  val data = List("scala", "java", "hadoop", "spark")
+  println(data.flatMap(_.toList))
+
+  println((1 to 100).foldLeft(0)(_ + _)) // 5050
+  println((0 /: (1 to 100))(_ + _)) // 5050
+
+
+  /** 模仿 /: 方法带两个参数的写法 */
+  class Test[B] {
+    def -: (z: B)(m: B): B = test(z)(m)
+    def test(x: B)(y: B): B = {
+      println(x + "    " + y)
+      x
+    }
+  }
+  val t = new Test[Int]
+  t.test(1)(2)
+
+  t.-:(1)(2) // 柯里化的写法
+  (1 -: t)(2) // 中置表达式类型写法
+
+}
