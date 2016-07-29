06a54a152ab141e09645d079ace897373b87f19b
zk_chs <zkchs1991@gmail.com>
2016-07-29 10:33:10 +0800

    callback function

---
+++ src/main/java/com/github/jdk/callback/Main.java
@@ -0,0 +1,26 @@
+package com.github.jdk.callback;
+
+/**
+ * Created by zk_chs on 16/7/29.
+ */
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        new TestClassBack().conpute(10, () -> System.out.println("this is callback!"));
+    }
+
+}
+
+class TestClassBack {
+
+    public void conpute (int n, ComputeCallBack callBack) throws InterruptedException {
+        System.out.println("this is n, and callback will run after 1s...");
+        Thread.sleep(1000);
+        callBack.onCompleteEnd();
+    }
+
+}
+
+interface ComputeCallBack {
+    void onCompleteEnd();
+}
